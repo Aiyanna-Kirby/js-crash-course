1dd7dba6429a6412a56adc66b6c9ef43231ee1c0
Aiyanna Smith-Taylor <aiyannast@outlook.com>
2026-09-07 22:01:08 -0500

    Array filter practice.

--- app.js
+++ app.js
@@ -1,7 +1,26 @@
 // DRY - Don't repeat yourself
 
-let arr = [20, 30, 40, 50, 100];
+// Array Filter Practice
+let arr = ["A+", "A", "FAIL"];
 
-let newArr = arr.filter((element) => element < 50);
+let newArr = arr.filter((element) => {
+  return element === "A+" || element === "A";
+});
 
 console.log(newArr);
+
+let arr2 = ["FAIL", "FAIL", "B"];
+
+let newArr2 = arr2.filter((element) => {
+  return element === "B";
+});
+
+console.log(newArr2);
+
+let arr3 = ["FAIL"];
+
+let newArr3 = arr3.filter((element) => {
+  return element === "";
+});
+
+console.log(newArr3);
