90bf39a848941a73a9e49498d1f65ae4b4751d19
Aiyanna Smith-Taylor <aiyannast@outlook.com>
2026-09-08 18:10:12 -0500

    Objects overview, combine with arrays.

--- app.js
+++ app.js
@@ -1,21 +1,22 @@
 // DRY - Don't repeat yourself
 
-//let arr = [1, 4, 9, 16]
+// Objects overview
+let users = [
+  {
+    username: "Aiyanna",
+    email: "iamaiyanna@gmail.com",
+    subscriptionStatus: "VIP",
+    discordId: "Aiyannakirby",
+    lessonsCompleted: [0, 1],
+  },
 
-//let newArray = arr.map ((element) => {
-//console.log(element)
-//return undefined
-//})
+  {
+    username: "David",
+    email: "whosedavida@gmail.com",
+    subscriptionStatus: "VIP",
+    discordId: "ABraggD",
+    lessonsCompleted: [0, 1, 2, 3],
+  },
+];
 
-//console.log(newArray)
-
-//Without Map Method
-
-let dollars = [1, 5, 10, 3];
-
-let cents = []
-
-for (let i = 0; i < dollars.length; ++i) {
-  console.log(dollars[i] * 100)}
-
-  console.log(cents)
+console.log(users[1].email);
