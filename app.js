62e9e61c74e9d7eeeabf4712ffeb0c4d7a0de98f
Aiyanna Smith-Taylor <aiyannast@outlook.com>
2026-09-07 21:43:55 -0500

    Shortest callback.

--- app.js
+++ app.js
@@ -1,11 +1,7 @@
 // DRY - Don't repeat yourself
 
-let arr = [20, 30, 40, 50, 100]
+let arr = [20, 30, 40, 50, 100];
 
-let newArr = arr.filter((element) => {
-  console.log(element)
-  if (element < 50)
-  return true
-})
+let newArr = arr.filter((element) => element < 50);
 
-console.log(newArr)
+console.log(newArr);
