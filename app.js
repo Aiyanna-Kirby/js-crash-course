915c8783c764b8df4ff27acb689c6da428c76d7d
Aiyanna Smith-Taylor <aiyannast@outlook.com>
2026-09-07 21:39:27 -0500

    Short way to do call back.

--- app.js
+++ app.js
@@ -2,13 +2,8 @@
 
 let arr = [20, 30, 40, 50, 100]
 
-// 1st element of array
-console.log(arr[0])
+let newArr = arr.filter(element => {
+  return element < 50
+})
 
-// Last element of array
-console.log(arr[arr.length - 1])
-
-// Add element onto end of array
-arr.push(200)
-
-console.log(arr)
+console.log(newArr)
