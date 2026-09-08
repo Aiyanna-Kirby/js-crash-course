31d9c7d7b6296d609f04746eb275307705b621ee
Aiyanna Smith-Taylor <aiyannast@outlook.com>
2026-09-08 18:29:31 -0500

    Logging someone in...

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 // DRY - Don't repeat yourself
 
-// Objects overview
+// How to log someone in
 let users = [
   {
     username: "Aiyanna",
     email: "iamaiyanna@gmail.com",
+    password: "wiggle",
     subscriptionStatus: "VIP",
     discordId: "Aiyannakirby",
     lessonsCompleted: [0, 1],
@@ -13,10 +14,27 @@ let users = [
   {
     username: "David",
     email: "whosedavida@gmail.com",
+    password: "whatareth00se",
     subscriptionStatus: "VIP",
     discordId: "ABraggD",
     lessonsCompleted: [0, 1, 2, 3],
   },
 ];
 
-console.log(users[1].email);
+function login(email, password) {
+  for (let i = 0; i < users.length; ++i) {
+    if (users[i].email === email) {
+      console.log(users[i]);
+      if (users[i].password === password) {
+        console.log("log them in");
+      }
+      else {
+        console.log('wrong pass, try again please')
+      }
+      return;
+    }
+  }
+  console.log('could not find email')
+}
+
+login("iamaiyanna@gmail.com", "wiggle");
