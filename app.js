0d9c6c494db81e2cc089f0e14f266fdcdd1d6d14
Aiyanna Smith-Taylor <aiyannast@outlook.com>
2026-09-08 21:17:52 -0500

    How to access elements.

--- app.js
+++ app.js
@@ -1,54 +1,7 @@
 // DRY - Don't repeat yourself
 
-// Creating a register function; hen push it
-let users = [
-  {
-    username: "Aiyanna",
-    email: "iamaiyanna@gmail.com",
-    password: "wiggle",
-    subscriptionStatus: "VIP",
-    discordId: "Aiyannakirby",
-    lessonsCompleted: [0, 1],
-  },
+// 1st way to access an element
+console.log(document.querySelector("#title"));
 
-  {
-    username: "David",
-    email: "whosedavida@gmail.com",
-    password: "whatareth00se",
-    subscriptionStatus: "VIP",
-    discordId: "ABraggD",
-    lessonsCompleted: [0, 1, 2, 3],
-  },
-];
-
-function login(email, password) {
-  for (let i = 0; i < users.length; ++i) {
-    if (users[i].email === email) {
-      console.log(users[i]);
-      if (users[i].password === password) {
-        console.log("log them in");
-      } else {
-        console.log("wrong pass, try again please");
-      }
-      return;
-    }
-  }
-  console.log("could not find email");
-}
-
-login("iamaiyanna@gmail.com", "wiggle");
-
-function register(user) {
-  users.push(user);
-}
-
-register({
-  username: "yanna",
-  email: "iamaiyanna@gmail.com",
-  password: "wiggle",
-  subscriptionStatus: "VIP",
-  discordId: "AiyannaKirby",
-  lessonsCompleted: [0, 1, 2],
-});
-
-console.log(users);
+// 2nd way to access an element
+console.log(document.getElementById("title"));
