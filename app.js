25e3dac2228abc738172c2fe3807f0a235b4a0d6
Aiyanna Smith-Taylor <aiyannast@outlook.com>
2026-09-08 21:37:20 -0500

    How target inside HTML.

--- app.js
+++ app.js
@@ -1,7 +1,3 @@
 // DRY - Don't repeat yourself
 
-// 1st way to access an element
-console.log(document.querySelector("#title"));
-
-// 2nd way to access an element
-console.log(document.getElementById("title"));
+document.querySelector('.title').innerHTML = 'Frontend Simplified'
