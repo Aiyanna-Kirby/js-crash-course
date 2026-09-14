b9ae3013658be49268116d31be534cd7298d4291
Aiyanna Smith-Taylor <aiyannast@outlook.com>
2026-09-13 19:26:58 -0500

    How to fetch and use promoises.

--- app.js
+++ app.js
@@ -1,6 +1,24 @@
 // DRY - Don't repeat yourself
 
-// Dark Mode Toggle
-function toggleDarkMode() {
-  console.log(document.querySelector("body").classList.toggle("dark-theme"));
+//console.log(fetch("https://jsonplaceholder.typicode.com/users/1"))
+const emailRef = document.querySelector(".email");
+
+// Way 1: Then
+// fetch("https://jsonplaceholder.typicode.com/users/1")
+//   .then((response) => {
+//     return response.json();
+//   })
+//   .then((data) => {
+//     console.log(data);
+//     emailRef.innerHTML = data.email
+//   });
+
+// Way 2: Async/Await
+async function main() {
+  const response = await fetch("https://jsonplaceholder.typicode.com/users/1");
+  const data = await response.json();
+  console.log(data);
+  emailRef.innerHTML = data.email;
 }
+
+main();
