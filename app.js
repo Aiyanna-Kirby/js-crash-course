8f8be654a8a9477d11ddc18aeeb7bb6c49da61e8
Aiyanna Smith-Taylor <aiyannast@outlook.com>
2026-09-13 21:27:19 -0500

    promise practice.

--- app.js
+++ app.js
@@ -1,24 +1,37 @@
 // DRY - Don't repeat yourself
 
-//console.log(fetch("https://jsonplaceholder.typicode.com/users/1"))
-const emailRef = document.querySelector(".email");
+const statusRef = document.querySelector(".status");
+const videoRef = document.querySelector(".video");
 
-// Way 1: Then
-// fetch("https://jsonplaceholder.typicode.com/users/1")
-//   .then((response) => {
-//     return response.json();
-//   })
-//   .then((data) => {
-//     console.log(data);
-//     emailRef.innerHTML = data.email
-//   });
+function getSubscriptionStatus() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve("FREE");
+    }, 2000);
+  });
+}
 
-// Way 2: Async/Await
+function getVideo(subscriptionStatus) {
+  return new Promise((resolve, reject) => {
+    if (subscriptionStatus === "VIP") {
+      resolve("show video");
+    } else if (subscriptionStatus === "FREE") {
+      resolve("show trailer");
+    } else {
+      reject("no video");
+    }
+  });
+}
+//Async/Await Method
 async function main() {
-  const response = await fetch("https://jsonplaceholder.typicode.com/users/1");
-  const data = await response.json();
-  console.log(data);
-  emailRef.innerHTML = data.email;
+  const status = await getSubscriptionStatus();
+  statusRef.innerHTML = status;
+  try {
+    console.log(await getVideo(status));
+  } catch (e) {
+    console.log(e);
+    videoRef.innerHTML = e;
+  }
 }
 
 main();
