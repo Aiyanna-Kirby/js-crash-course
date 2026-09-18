4c4eae7a494296c142b77de73ff08caf2ffd606d
Aiyanna Smith-Taylor <aiyannast@outlook.com>
2026-09-18 18:51:20 -0500

    converting values inside.

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 // DRY - Don't repeat yourself
 
-function reverseString(string) {
-  return string.split('').reverse().join('')
+function convertToZeros(arr) {
+  let newArr = [];
+  for (let i = 0; i < arr.length; ++i) {
+    newArr[i] = 0;
+  }
+  return newArr;
 }
 
-console.log(reverseString('yurt'))
+console.log(convertToZeros([12, 4, 7]));
