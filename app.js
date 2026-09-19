b1f2ad3853998d43feb7c17f3a243a9943db7799
Aiyanna Smith-Taylor <aiyannast@outlook.com>
2026-09-18 19:20:35 -0500

    Using filter.

--- app.js
+++ app.js
@@ -1,12 +1,7 @@
 // DRY - Don't repeat yourself
 
 function removeApples(arr) {
-  let newArr = [];
-  for (let i = 0; i < arr.length; ++i) {
-    if (arr[i] !== 'apple') 
-      newArr.push(arr[i])
-    }
-    return newArr
-  }
+  return arr.filter(elm => elm !== 'apple')
+}
 
-console.log(removeApples(['banana', 'apple', 'orange', 'apple']));
+console.log(removeApples(["banana", "apple", "orange", "apple"]));
