84619ed55f4e5d56057a8fdfc8d1fd13f18ee5ea
Aiyanna Smith-Taylor <aiyannast@outlook.com>
2026-09-19 17:35:19 -0500

    Sorting numbers in order.

--- app.js
+++ app.js
@@ -1,33 +1,7 @@
 // DRY - Don't repeat yourself
 
-//my attempt
-// function showRating(rating) {
-//   for (let i = 0; i < rating.length; ++i) {
-//     if (rating > 0) {
-//       console.log("*");
-
-//     } else if (rating < 1) {
-//       console.log(".");
-//     }
-//   }
-// }
-
-//console.log(showRating("3.7"));
-
-//Actual
-
-function showRating(rating) {
-  let ratings = "";
-  for (let i = 0; i < Math.floor(rating); ++i) {
-    ratings = ratings + "*";
-    if (i !== Math.floor(rating) - 1) {
-      ratings = ratings + " ";
-    }
-  }
-  if (Number.isInteger(rating)) {
-    ratings = ratings + " ?";
-  }
-  return ratings;
+function sortLowToHigh(numbers) {
+  return numbers.sort((a, b) => a - b);
 }
 
-console.log(showRating(5.5));
+console.log(sortLowToHigh([4, 27, 3, 37, 100]));
